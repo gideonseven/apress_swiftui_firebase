f97d74145a2966377eaec95efc6af9c616b441d5
Gideon Tobing <gideon.tobing@gmail.com>
2025-11-11 03:09:23 +1000

    init ch3

---
+++ ch3/Note/Note/NoteApp.swift
@@ -0,0 +1,31 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Gideon Tobing on 11/11/2025.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+
+@main
+struct NoteApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
