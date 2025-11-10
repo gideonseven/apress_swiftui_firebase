0f0fb0034a020ecf4b725f1ba0796e9ab973d01c
Gideon Tobing <gideon.tobing@gmail.com>
2025-11-11 02:39:23 +1000

    add ch2 note project files

---
+++ ch2/Note/Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Gideon Tobing on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ch2/Note/NoteTests/NoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTests.swift
+//  NoteTests
+//
+//  Created by Gideon Tobing on 11/11/2025.
+//
+
+import Testing
+@testable import Note
+
+struct NoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
