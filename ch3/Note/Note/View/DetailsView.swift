116f27692e1ad49ae9fb05b2700ce6ce9f7d2d58
Gideon Tobing <gideon.tobing@gmail.com>
2025-11-11 15:26:13 +1000

    update detailview

--- ch3/Note/Note/View/DetailsView.swift
+++ ch3/Note/Note/View/DetailsView.swift
@@ -9,43 +9,42 @@ import SwiftUI
 struct DetailsView: View {
     @State private var presentAlert = false
     @State private var titleText: String = ""
-    @ObservedObject private var viewModel = NoteViewModel()
+    @StateObject private var viewModel = NoteViewModel()
     var note: Note
     
     var body: some View {
-            ScrollView {
-                VStack {
-                    Text("\(note.title ?? "")")
-                        .font(.system(size: 22, weight: .regular))
-                        .padding()
-                    
-                    Spacer()
+        ScrollView {
+            VStack {
+                Text("\(note.title ?? "")")
+                    .font(.system(size: 22, weight: .regular))
+                    .padding()
+                
+                Spacer()
+            }
+        }
+        .navigationTitle("Details")
+        .toolbar {
+            ToolbarItemGroup(placement: .confirmationAction) {
+                Button {
+                    presentAlert = true
+                } label: {
+                    Text("Edit").bold()
                 }
             }
-            .navigationTitle("Details")
-            .toolbar{
-                ToolbarItemGroup(placement: .confirmationAction){
-                    Button {
-                        presentAlert = true
-                    } label: {
-                        Text ("Edit").bold()
-                    }.alert("Note", isPresented: $presentAlert, actions: {
-                        TextField("\(note.title ?? "")", text: $titleText)
-                        Button("Update", action: {
-                            //Update data and erase the text
-                            self.viewModel.updateData(title: titleText, id: note.id ?? "")
-                            titleText = ""
-                        })
-                        Button("Cancel", role: .cancel, action: {
-                            presentAlert = false
-                            titleText = ""
-                        })
-                    }, message: {
-                        Text("Write your new note")
-                    })
-                }
-            }
-        
+        }
+        .alert("Note", isPresented: $presentAlert, actions: {
+            TextField("\(note.title ?? "")", text: $titleText)
+            Button("Update", action: {
+                self.viewModel.updateData(title: titleText, id: note.id ?? "")
+                titleText = ""
+            })
+            Button("Cancel", role: .cancel, action: {
+                presentAlert = false
+                titleText = ""
+            })
+        }, message: {
+            Text("Write your new note")
+        })
     }
 }
 
