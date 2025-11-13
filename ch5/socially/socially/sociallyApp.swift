1e8382b102373daf86ef6d17a792548e0b8efd25
Gideon Tobing <gideon.tobing@gmail.com>
2025-11-14 00:44:42 +1000

    init ch5 socially project

---
+++ ch5/socially/socially/sociallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sociallyApp.swift
+//  socially
+//
+//  Created by gideon tobing on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct sociallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
